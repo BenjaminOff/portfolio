1a355883da1ebac705d07dd9002f82d81c8e0d9e
Benjamin C <116800361+BenjaminOff@users.noreply.github.com>
2023-05-23 20:04:57 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,30 +2,30 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'BenjaminOff', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
+      forks: true, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: '',
+    twitter: 'kunio_bg',
+    mastodon: '',
     facebook: '',
     instagram: '',
     dribbble: '',
     behance: '',
     medium: 'arifszn',
-    dev: 'arifszn',
+    dev: 'BenjaminOff',
     stackoverflow: '', // format: userid/username
     skype: '',
     telegram: '',
-    website: 'https://arifszn.com',
+    website: 'https:/kunioweb.site',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'contact@kunioweb.site',
   },
   resume: {
     fileUrl:
