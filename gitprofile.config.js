3ad2a03b94b199115c0d80dea9b3a742b0e2e241
Benjamin C <116800361+BenjaminOff@users.noreply.github.com>
2023-05-23 20:55:27 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -78,13 +78,7 @@ const config = {
         'Gamme de robot discord.',
     },
 
-  
-  // Display blog posts from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'BenjaminOff', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
-  },
+
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
@@ -153,6 +147,12 @@ const config = {
     },
   },
 
- 
+  // Optional Footer. Supports plain text or HTML.
+  footer: `Made with <a 
+      class="text-primary" href="https://github.com/arifszn/gitprofile"
+      target="_blank"
+      rel="noreferrer"
+    >GitProfile</a> and ❤️`,
+};
 
 export default config;
